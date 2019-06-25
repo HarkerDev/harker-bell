7320443170287db4d4a2a2f5f539d4e3440205df
Bowen Yin <bowenvictoryin@gmail.com>
2019-06-24 22:33:42 -0700

    Assign route to settings view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid style="min-width: 932px;">
     <v-layout row justify-center>
-      <schedule-day v-for="i in 5" :key="i" :id="'day'+i"></schedule-day>
+      <schedule-day v-for="i in 5" :key="i" :id="'day'+i" :schedule="schedule" :view="view"></schedule-day>
     </v-layout>
   </v-container>
 </template>
@@ -13,11 +13,61 @@ export default {
   components: {
     ScheduleDay,
   },
+  created() {
+    this.updateView();
+  },
   data() {
     return {
-      
+      view: "day",
+      schedule: [
+        {
+          name: "P1",
+          start: new Date("2019-06-21T08:00Z"),
+          end: new Date("2019-06-21T09:25Z")
+        },
+        {
+          name: "P2",
+          start: new Date("2019-06-21T09:35Z"),
+          end: new Date("2019-06-21T11:00Z")
+        },
+        {
+          name: "Lunch",
+          start: new Date("2019-06-21T11:10Z"),
+          end: new Date("2019-06-21T11:55Z")
+        },
+        {
+          name: "Soph Mtg.",
+          start: new Date("2019-06-21T11:10Z"),
+          end: new Date("2019-06-21T11:25Z")
+        },
+        {
+          name: "Lunch",
+          start: new Date("2019-06-21T11:25Z"),
+          end: new Date("2019-06-21T11:55Z")
+        },
+        {
+          name: "P3",
+          start: new Date("2019-06-21T12:00Z"),
+          end: new Date("2019-06-21T13:25Z")
+        },
+        {
+          name: "P4",
+          start: new Date("2019-06-21T13:35Z"),
+          end: new Date("2019-06-21T15:00Z")
+        },
+      ],
     };
   },
+  methods: {
+    updateView() {
+      //this.$emit("route-changed");
+    }
+  },
+  watch: {
+    $route() {
+      this.updateView();
+    }
+  }
 };
 </script>
 
