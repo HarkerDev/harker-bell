3685c8ba26d62032686d0f7281a800c5710d3f3d
Bowen Yin <bowenvictoryin@gmail.com>
2019-08-13 13:44:10 -0700

    Fix multiple click events firing on iOS Safari

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
       </v-menu>
       <v-tooltip bottom open-delay="500" transition="scale-transition" origin="top center">
         <template v-slot:activator="{on}">
-          <v-btn class="hidden-print-only mr-2" icon v-on="on" @click="clickEvent();nextOrPrevious(true)">
+          <v-btn class="hidden-print-only mr-2" icon v-on="on" @click.once="clickEvent();nextOrPrevious(true)">
             <v-icon>chevron_right</v-icon>
           </v-btn>
         </template>
