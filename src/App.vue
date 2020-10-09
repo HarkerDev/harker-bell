16f5dbd7d80e259d0b88ae1f3745516c367a056c
Bowen Yin <bowenvictoryin@gmail.com>
2020-10-09 23:47:48 +0000

    Add link option for Clubs; Release v2.5.1

--- src/App.vue
+++ src/App.vue
@@ -185,7 +185,7 @@
           <v-row>
             <v-col>
               <period-setting v-for="i in 7" :key="i" :num="i" :settings="settings"></period-setting>
-              <link-setting v-for="x in ['Advisory', 'Class Meeting', 'Office Hours']" :key="x" :name="x" :settings="settings"></link-setting>
+              <link-setting v-for="x in ['Advisory', 'Class Meeting', 'Office Hours', 'Clubs']" :key="x" :name="x" :settings="settings"></link-setting>
             </v-col>
           </v-row>
           <v-row>
