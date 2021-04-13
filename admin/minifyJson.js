032a92b60f676e3da55bfd0e0a3d2b862c57e47d
Bowen Yin <bowenvictoryin@gmail.com>
2021-04-13 02:57:01 +0000

    Minify JSON

---
+++ admin/minifyJson.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const files = ["../public/data/schedules.json"];
+for (const file of files) {
+  const data = JSON.stringify(JSON.parse(fs.readFileSync(file, "utf8")));
+  fs.writeFileSync(file, data);
+}
