932fa2eb08487de9edfcd262a22a9357c4ea90cc
Bowen Yin <bowenvictoryin@gmail.com>
2021-04-13 02:27:19 +0000

    Reformat schedules

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="sticky top-0 bg-white bg-opacity-90">
-    text
+  <div class="sticky top-0 h-14 flex justify-center bg-white bg-opacity-90">
+    <div class="material-icons-outlined flex items-center mx-2">chevron_left</div>
+    <div class="material-icons-outlined flex items-center mx-2">event</div>
+    <div class="material-icons-outlined flex items-center mx-2">chevron_right</div>
+    <div class="flex items-center mx-2">Harker Bell Schedule</div>
+    <div class="material-icons-outlined flex items-center mx-2">settings</div>
   </div>
-  <div>
+  <div class="flex justify-center items-center">Message</div>
+  <div class="flex justify-center">
+    <div class="w-36 h-96 border-gray-500 border-2"></div>
+  </div>
+  <!-- <div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + Vite" />
     <input type="text" placeholder="Text">
     <input type="checkbox">
     <h1>text</h1>
     <div style="height: 200px"></div>
-  </div>
+  </div> -->
 </template>
 
 <script setup>
