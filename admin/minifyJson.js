b28f90046d80dda49cd19c334c1e92a4d1cc2077
Bowen Yin <bowenvictoryin@gmail.com>
2021-04-13 03:11:05 +0000

    Fixed buggy bug

--- admin/minifyJson.js
+++ admin/minifyJson.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const files = ["../public/data/schedules.json"];
+const files = ["./public/data/schedules.json"];
 for (const file of files) {
   const data = JSON.stringify(JSON.parse(fs.readFileSync(file, "utf8")));
   fs.writeFileSync(file, data);
